340ca66c6c6bfeb82447150a9c969f038f956f02
szd <szymon.piotr.duda@gmail.com>
2020-11-28 19:06:23 +0100

    Updates

---
+++ precache-manifest.35071c8f3beec7c3ac20dd9d19e4db44.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f79bfdb71d1f1d74eba142f7223d3cb9",
+    "url": "/remonter/index.html"
+  },
+  {
+    "revision": "46ec7ff5cc96d7e2c1f4",
+    "url": "/remonter/static/js/2.2568d237.chunk.js"
+  },
+  {
+    "revision": "7c8c498c154c5c7fc14cf0ad148613b4",
+    "url": "/remonter/static/js/2.2568d237.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f625d615d862626d43b1",
+    "url": "/remonter/static/js/main.5c272245.chunk.js"
+  },
+  {
+    "revision": "5ceef785d0eee8820caa",
+    "url": "/remonter/static/js/runtime-main.b02a96ab.js"
+  },
+  {
+    "revision": "0e3e08bcb441c2cb39bdfff7f56f06a2",
+    "url": "/remonter/static/media/add.0e3e08bc.svg"
+  },
+  {
+    "revision": "b840e171a500c69863f00f6b4056b486",
+    "url": "/remonter/static/media/close.b840e171.svg"
+  },
+  {
+    "revision": "62d23dd4052d525e8de4a6f7c58865b6",
+    "url": "/remonter/static/media/exclamation.62d23dd4.svg"
+  },
+  {
+    "revision": "55f6849429de279d473c459c7ead3249",
+    "url": "/remonter/static/media/hamburger.55f68494.svg"
+  },
+  {
+    "revision": "db5d8122836517965b6e0247a3a9691c",
+    "url": "/remonter/static/media/search.db5d8122.svg"
+  },
+  {
+    "revision": "52bd8411f8aea9dba1e636497a1c3d3e",
+    "url": "/remonter/static/media/unfold.52bd8411.svg"
+  }
+]);
